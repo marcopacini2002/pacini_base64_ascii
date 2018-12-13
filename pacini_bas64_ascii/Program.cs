466bf23ed1ad36531c1adc1c933cddafb34f1ea8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-13 09:46:53 +0100

    ho riscritto il programma

---
+++ pacini_bas64_ascii/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacini_bas64_ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "man";
+            string base64EncodedData;
+
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            for (int i = 0; i < plainTextBytes.Length; i++)
+            {
+                Console.WriteLine(plainTextBytes[i]);
+                Console.WriteLine(Convert.ToString(plainTextBytes[i], 2));
+            }
+
+            base64EncodedData = Convert.ToBase64String(plainTextBytes);
+            Console.WriteLine(base64EncodedData);
+
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            Console.WriteLine(plainText);
+
+            Console.ReadLine();
+        }
+    }
+}
