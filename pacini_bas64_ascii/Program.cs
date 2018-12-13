8fee48287501464cc6bcb03e772bf55fe1a61866
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-13 09:50:29 +0100

    ho sostituito var con byte[]

--- pacini_bas64_ascii/Program.cs
+++ pacini_bas64_ascii/Program.cs
@@ -15,7 +15,7 @@ namespace pacini_bas64_ascii
             Console.WriteLine("INserisci il testo da convertire");
             plainText = Console.ReadLine();
 
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             for (int i = 0; i < plainTextBytes.Length; i++)
             {
                 Console.WriteLine(plainTextBytes[i]);
@@ -25,7 +25,7 @@ namespace pacini_bas64_ascii
             base64EncodedData = Convert.ToBase64String(plainTextBytes);
             Console.WriteLine(base64EncodedData);
 
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+           byte[] base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
             Console.WriteLine(plainText);
 
